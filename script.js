3dc05203fac7894fb316ef756012319489b46c3d
Nathan Grissett <128097313+nathangrissett@users.noreply.github.com>
2024-10-26 12:14:19 -0500

    Add first draft

---
+++ script.js
@@ -0,0 +1,228 @@
+// Gameboard
+
+function Gameboard() {
+    const rows = 3;
+    const columns = 3;
+    const board = [];
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < columns; j++) {
+            board[i].push(Cell());
+        }
+    }
+
+    const getBoard = () => board;
+
+
+    const dropMarker = (column, row, player) => {
+        if (board[column][row].getValue() != 0) {
+            return false;
+        }
+        board[column][row].addMark(player);
+        return true;
+    };
+    
+
+    const printBoard = () => {
+        const boardWithCellValues = board.map((row) =>
+            row.map((cell) => cell.getValue()))
+        console.log(boardWithCellValues);
+    };
+
+    const checkWinner = (player) => {
+        for (let row = 0; row < 3; row++) {
+            if (board[row].every(cell => cell.getValue() === player))
+                return true;
+        }
+
+        for (let col = 0; col < 3; col++) {
+            if (board.every(row => row[col].getValue() === player)) {
+                return true;
+            }
+        }
+
+        if (board[0][0].getValue() === player &&
+            board[1][1].getValue() === player &&
+            board[2][2].getValue() === player) {
+            return true;
+        }
+
+        if (board[0][2].getValue() === player &&
+            board[1][1].getValue() === player &&
+            board[2][0].getValue() === player) {
+            return true;
+        }
+
+        return false;
+    };
+
+    return { getBoard, dropMarker, printBoard, checkWinner };
+}
+
+// Cell
+
+function Cell() {
+    let value = 0;
+
+    const addMark = (player) => {
+        value = player;
+    };
+
+    const getValue = () => value;
+
+    return {
+        addMark,
+        getValue
+    };
+}
+
+// Game flow
+
+let gameOver = false;
+
+function GameController(
+    playerOneName = "Player One",
+    playerTwoName = "Player Two"
+) {
+    const board = Gameboard();
+
+    const players = [
+        {
+            name: playerOneName,
+            marker: 1
+        },
+        {
+            name: playerTwoName,
+            marker: 2
+        }
+    ];
+
+    let activePlayer = players[0];
+
+    gameOver = false;
+
+    const switchPlayerTurn = () => {
+        activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    };
+
+    const getActivePlayer = () => activePlayer;
+
+    const printNewRound = () => {
+        board.printBoard();
+        if (!gameOver) {
+            console.log(`${getActivePlayer().name}'s turn`)
+        }
+    };
+
+    const playRound = (column, row) => {
+
+        if (gameOver) {
+            console.log('Game is over! Start a new game to play again.');
+            return;
+        }
+
+        console.log(
+            `Dropping ${getActivePlayer().name}'s marker into column ${column} row ${row}...`
+        );
+
+        if (!board.dropMarker(column, row, getActivePlayer().marker)) {
+            console.log("That position is already taken! Try again.")
+            return;
+        }
+
+        //Winner Logic
+
+        if (board.checkWinner(getActivePlayer().marker)) {
+            gameOver = true;
+            board.printBoard();
+            console.log(`${getActivePlayer().name} wins!`);
+            return;
+        }
+        
+        //Tie Logic
+
+
+        const isBoardFull = () => {
+            return board.getBoard().every(row =>
+                row.every(cell => cell.getValue() !== 0)
+            );
+        };
+
+        if (isBoardFull()) {
+            console.log("It's a tie!");
+            board.printBoard();
+            gameOver = true;
+            return;
+        }
+
+        switchPlayerTurn();
+        printNewRound();
+    };
+
+    //Initial play game message
+    printNewRound();
+
+    return {
+        playRound,
+        getActivePlayer
+    };
+}
+
+//DOM controls
+
+const newGame = document.querySelector("#new-game");
+let playerOneText = document.querySelector("#player1").value;
+let playerTwoText = document.querySelector("#player2").value;
+const changePlayer1 = document.querySelector("#change-player1");
+const changePlayer2 = document.querySelector("#change-player2");
+let game;
+const player1Marker = "X";
+const player2Marker = "O";
+let count = 0;
+const squares = document.querySelectorAll('.square');
+const rows = document.querySelectorAll('div.row');
+let gameEnd = true;
+
+function isOdd(num) { return num % 2; }
+
+newGame.addEventListener('click', function() {
+    game = GameController(playerOneText, playerTwoText);
+    count = 0;
+    for (const square of squares) {
+        square.textContent = "";
+    }
+    gameEnd = true;
+    for (const row of rows) {
+        row.style.visibility = "visible";
+    }
+});
+
+[changePlayer1, changePlayer2].forEach((button, index) => {
+    button.addEventListener('click', function() {
+        const playerInput = document.querySelector(`#player${index + 1}`);
+        if (index === 0) {
+            playerOneText = playerInput.value;
+        } else {
+            playerTwoText = playerInput.value;
+        }
+    });
+});
+
+for (const square of squares) {
+    square.addEventListener('click', function() {
+        const [row, col] = square.id.split('-').map(Number);
+
+        if (square.textContent == "" && gameEnd) {
+            game.playRound(row, col);
+            if (!isOdd(count) && gameEnd) {
+                square.textContent = player1Marker;
+            } else if (isOdd(count) && gameEnd) {
+                square.textContent = player2Marker;
+            }
+            count++;
+        }
+        if (gameOver == true) {
+            gameEnd = false;
+        }
+    });
+}
